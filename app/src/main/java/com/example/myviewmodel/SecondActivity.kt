11d46757ce32d1ca8a818d1acf7da689f4d386af
yokato <you1katou@gmail.com>
2022-01-10 23:32:05 +0900

    Androidでコードからviewを追加する - Qiita https://qiita.com/asukahime1021/items/8034341b55ebe4f7cc5c

--- app/src/main/java/com/example/myviewmodel/SecondActivity.kt
+++ app/src/main/java/com/example/myviewmodel/SecondActivity.kt
@@ -3,6 +3,12 @@ package com.example.myviewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.myviewmodel.R.color.white
 import com.example.myviewmodel.ui.main.MainFragment
 
 class SecondActivity : AppCompatActivity() {
@@ -23,5 +29,17 @@ class SecondActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate: car = " + car + ", .name = "
                 + car.name + ", .id = " + car.id)
         Log.d(TAG, "onCreate: car = $car, .name = ${car.name} , id = ${car.id}")
+
+        /* XMLではなく、コードでTextView・ImageViewを下方に追加 */
+        val layout = findViewById<LinearLayout>(R.id.container)
+        val textView = TextView(applicationContext)
+        textView.text = "hogehoge"
+        textView.setTextColor(resources.getColor(white))
+        layout.addView(textView)
+
+        val imageView = ImageView(applicationContext)
+        imageView.setImageResource(R.drawable.ic_launcher_foreground)
+//        imageView.foregroundGravity = Gravity.END
+        layout.addView(imageView)
     }
 }
