5f8e5b847fd5d90d5303b62a17155639f14a94a9
yokato <you1katou@gmail.com>
2022-01-10 23:59:46 +0900

    コードでレイアウト生成しViewを追加し、それを今のレイアウトにaddView
    [Android] レイアウトファイルを使わないでコードでTextViewを作る https://akira-watson.com/android/textview-2.html

--- app/src/main/java/com/example/myviewmodel/SecondActivity.kt
+++ app/src/main/java/com/example/myviewmodel/SecondActivity.kt
@@ -26,20 +26,37 @@ class SecondActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
         Log.d(TAG, "onCreate: intent = $intent")
         val car = intent.getSerializableExtra("car") as DiscoveredCar
-        Log.d(TAG, "onCreate: car = " + car + ", .name = "
-                + car.name + ", .id = " + car.id)
+        Log.d(
+            TAG, "onCreate: car = " + car + ", .name = "
+                    + car.name + ", .id = " + car.id
+        )
         Log.d(TAG, "onCreate: car = $car, .name = ${car.name} , id = ${car.id}")
 
         /* XMLではなく、コードでTextView・ImageViewを下方に追加 */
+        //Androidでコードからviewを追加する - Qiita https://qiita.com/asukahime1021/items/8034341b55ebe4f7cc5c
         val layout = findViewById<LinearLayout>(R.id.container)
-        val textView = TextView(applicationContext)
-        textView.text = "hogehoge"
-        textView.setTextColor(resources.getColor(white))
-        layout.addView(textView)
+        val tv = TextView(applicationContext)
+        tv.text = "hogehoge"
+        tv.setTextColor(resources.getColor(white))
+        layout.addView(tv)
 
         val imageView = ImageView(applicationContext)
         imageView.setImageResource(R.drawable.ic_launcher_foreground)
 //        imageView.foregroundGravity = Gravity.END
         layout.addView(imageView)
+
+        /* [Android] レイアウトファイルを使わないでコードでTextViewを作る
+         https://akira-watson.com/android/textview-2.html */
+        // LinearLayoutのインスタンス作成
+        val linearLayout = LinearLayout(this)
+        // LinearLayoutの特徴としてorientation(方向)を決る
+        linearLayout.orientation = LinearLayout.VERTICAL
+        // setContentViewに設定
+//        setContentView(linearLayout)
+        val textView = TextView(this)
+        textView.text = "Test TextView"
+        linearLayout.addView(textView)
+        layout.addView(linearLayout)
+
     }
 }
